25ecaf1f593859362c07392bb3a6a14b64933fd1
Benson Caleb <115053551+KallyBee@users.noreply.github.com>
2023-12-15 14:16:18 +0100

    Create Programming assignment unit 1.py

---
+++ YEAR 1/Programming assignment unit 1.py
@@ -0,0 +1,49 @@
+
+1A) Code : Print (“Benson Caleb)
+        Output error : SyntaxError: EOL while scanning string literal.
+       # Explanation : if one of the quotation marks is left out or both while printing a name, it will show a syntax error message indicating that quotation marks has been omitted because python language is case sensitive.
+
+1B) Code : 2 * 3
+       Output : 6
+      # Explanation: The operator * is a Mathematical expression of multiplication, that is if the operand of both 2 and 3 are to be multiplied together, we get an output of 6
+       Code : 2 ** 3
+       Output : 8
+     #  Explanation : The operator ** is a Mathematical expression of raised to the power, that is  if the operand of 2 is to be raised to the power of the operand 3, we get an output of 8.
+
+1C) No , it is not possible to justify an integer 09 because the leading zeros in decimal integer literals are not permitted; instead it is preferable to use an 0o prefix for octal integers
+
+1D) code : print (type (“67”))
+      Output:   with the quotation mark of double quotes, the python interpreter see it as a strings 
+ 
+ 
+ 
+Code : print (type (67))
+
+#Explantion :  without the quotation mark of double quotes, the python interpreter see it as    an integer 
+
+2A) my_age = 20
+Answer = my_age * 2
+Print (answer)
+ 
+
+2B) city = "Ikorodu\n"
+country = "Nigeria\n"
+continent = "Africa\n "
+
+print (city +  country + continent) 
+
+2C) starting_date = “ Examination date schedule begins on 16th of November 2023\n “
+ending_date =  “and ends on the 10th of January 2024 “
+
+Schedule = starting_date + ending_date
+Print (schedule)
+
+ 
+
+2D) temp_nigeria = “ 32°C “
+
+Print (“ Nigeria Temperature currently now is” + temp_nigeria)
+
+ 
+
+
