6f11445bb0c7d6eed1666eb417e283e5f01df42e
Benson Caleb <115053551+KallyBee@users.noreply.github.com>
2023-12-15 14:27:15 +0100

    Create Programming assignment unit 4.py

---
+++ YEAR 1/Programming assignment unit 4.py
@@ -0,0 +1,60 @@
+
+
+import math
+
+
+
+def right_angle_triangle(opp, adj) :
+
+    hypo = math.sqrt(opp ** 2  + adj ** 2)
+
+    return hypo
+
+     
+
+print(right_angle_triangle(3,4))
+
+
+
+ 
+
+
+
+import math
+
+
+
+def right_angle_triangle(opp, adj) :
+
+    hypo = math.sqrt(opp ** 2  + adj ** 2)
+
+   return hypo
+
+
+
+print(right_angle_triangle(15,17))
+
+
+
+
+
+import math
+
+
+
+def right_angle_triangle(opp, adj) :
+
+
+
+  hypo = math.sqrt(opp ** 2  + adj ** 2)
+
+  return hypo
+
+
+
+print(right_angle_triangle(9,7))
+
+
+
+
+
